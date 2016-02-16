4de5d8555a767943a6fce8c1384a9d8d167bd14c
Robert Carswell <rcarswel@westga.edu>
2016-02-16 13:51:46 -0500

    Modified the WebsiteListActivity class.

--- app/src/main/java/edu/westga/cs6242/masterdetailflow/WebsiteListActivity.java
+++ app/src/main/java/edu/westga/cs6242/masterdetailflow/WebsiteListActivity.java
@@ -4,21 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.List;
 
 import edu.westga.cs6242.masterdetailflow.dummy.DummyContent;
-
-import java.util.List;
 
 /**
  * An activity representing a list of Websites. This activity
@@ -91,7 +89,7 @@ public class WebsiteListActivity extends AppCompatActivity {
         public void onBindViewHolder(final ViewHolder holder, int position) {
             holder.mItem = mValues.get(position);
             holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
+            holder.mContentView.setText(mValues.get(position).website_name);
 
             holder.mView.setOnClickListener(new View.OnClickListener() {
                 @Override
